fa5b37f02c2fba2906670a08de460e316493d037
Darryl Charles <dk.charles@ulster.ac.uk>
2023-10-16 17:19:13 +0100

    Animator added and animations linked to inputs via code

--- Assets/_GDAD/Scripts/PlayerMovement.cs
+++ Assets/_GDAD/Scripts/PlayerMovement.cs
@@ -9,18 +9,33 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] int playerSpeed = 5;
     Vector2 movement;
     Rigidbody2D rb;
+    Animator animator;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         rb.AddForce(movement * playerSpeed);
     }
-
+    
     void OnMovement(InputValue value)
-    {
+    {   // Unity Input System function for movement - called from PlayerInput component
+
         movement = value.Get<Vector2>();
+
+        // if the player is moving (movement value is not zero), set the animator parameters
+        if (movement != Vector2.zero)   
+        { 
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y);
+
+            animator.SetBool("isWalking", true);
+        } else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 }
